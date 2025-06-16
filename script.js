a2dcc06b60bd8fab02ba6fe3395a9ebd320a459b
Isaac Nascimento Sales <121314776+isaacsaless@users.noreply.github.com>
2025-06-16 16:12:28 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 $(document).ready(function(){
-	$('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4').addClass('loaded');
-	
-	$('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4').click(function(){
-		$(this).toggleClass('open');
-	});
+    $('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4').addClass('loaded');
+
+    $('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4').click(function(){
+        $(this).toggleClass('open');
+    });
+
+    $(document).on('touchmove', function(e) {
+        if (e.touches.length === 1) {
+            let touch = e.touches[0];
+            if (Math.abs(touch.clientX - touch.screenX) > Math.abs(touch.clientY - touch.screenY)) {
+                e.preventDefault();
+            }
+        }
+    });
 });
